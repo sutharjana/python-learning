d17fb27a16ae9607ff9bc83e129c0dc91d9fb868
sutharjana <sutharja2006@gmail.com>
2026-06-14 22:53:58 +0530

    feat: add loop to print numbers from 1 to 5 in hello.py

--- hello.py
+++ hello.py
@@ -5,3 +5,6 @@ print(a!=b)
 print(a==b)     
 print(a+b*a-b/a)
 print()
+for i in range(1, 6):
+    print(i)
+    
--- jana.py
+++ jana.py
@@ -4,3 +4,7 @@ c=float(input("enter the velocity"))
 e=0.5*m*c*c
 print ("kinetic energy is" , e)
 print(f"the energy is {e}" )
+while True:
+    print("hello")
+    break
+    
