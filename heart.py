1b440ed3b8ced2c1460fdac2417e2ab500742a0b
sutharjana <sutharja2006@gmail.com>
2026-06-18 23:23:22 +0530

    feat: add heart pattern printing functionality in heart.py

--- heart.py
+++ heart.py
@@ -1,1 +1,9 @@
 n=5
+for i in range(n):
+    for j in range(2*n-1):
+        if (i==0 or i==n-1 or j==0 or j==2*n-2 or i+j==n-1 or i-j==n-1 +n):
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+
+    print()
